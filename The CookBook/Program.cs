2c20ccfe881dfc5c39c8634e1a3ff309acef843e
SENSEI0Death <lordchadow10@gmail.com>
2020-12-03 21:57:28 +0100

    changing the txt file and reading code

--- The CookBook/Program.cs
+++ The CookBook/Program.cs
@@ -9,24 +9,115 @@ namespace The_CookBook
 {
     class Program
     {
-        static public void addRecipe()
+        public static string[] lines = System.IO.File.ReadAllLines(@"C:\Users\ZAKARIA\source\repos\The CookBook\recipes.txt");
+        public static void addRecipe()
         {
             Console.Clear();
             Console.WriteLine("--------Add New Recipe--------");
-            Console.WriteLine("enter your recipe name:");
+            Console.Write("enter your recipe name:");
             string recipe = Console.ReadLine();
-            Console.WriteLine("enter the category of the recipe:");
+            Console.Write("enter the category of the recipe:");
             string cat = Console.ReadLine();
-            //obj need for the ingredients 
-        }
-        static public void SearchRecipe()
-        {
-            Console.Clear();
-            Console.WriteLine("enter your recipe you looking for:");
-            string SearchR = Console.ReadLine();
+            Console.WriteLine("The Ingredinets(joinning by commas ',') :");//salt,pepper,duck,tomatos,potatos....
+            string InputIngreds = Console.ReadLine();
+            string[] Ingreds = InputIngreds.Split(new char[] { ',' });
+
+            Console.Write("The description:");
+            string descript = Console.ReadLine();
 
         }
-        static public void menu()
+        public static void AllRecipes()
+        {
+            Console.Clear();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string recipename = line.Split('@')[0];
+                Console.WriteLine(recipename);
+            }
+        }
+        public static void SearchRecipe()
+        {
+            Console.Clear();
+            Console.Write("recipe you looking for:");
+            string SearchR = Console.ReadLine();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string recipename = line.Split('@')[0];
+                string category = line.Split('@')[1].Split('$')[0];
+                string ingreds = line.Split('@')[1].Split('$')[1].Split('#')[0];
+                string description = line.Split('@')[1].Split('$')[1].Split('#')[1];
+                if (SearchR == recipename)
+                {
+                    Console.WriteLine($"{recipename} , {category} , {ingreds} , {description}");
+                }
+                else
+                {
+                    Console.WriteLine("We Dont Have any recipe match!");
+                    break;
+                }
+            }
+        }
+        public static void SearchCategory()
+        {
+            Console.Clear();
+            Console.Write("Category you looking for:");
+            string SearchC = Console.ReadLine();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string recipename = line.Split('@')[0];
+                string category = line.Split('@')[1].Split('$')[0];
+                string ingreds = line.Split('@')[1].Split('$')[1].Split('#')[0];
+                string description = line.Split('@')[1].Split('$')[1].Split('#')[1];
+                if (SearchC == category)
+                {
+                    Console.WriteLine($"{recipename} , {category} , {ingreds} , {description}");
+                }
+                else
+                {
+                    Console.WriteLine("We Dont Have any category match!");
+                    break;
+                }
+            }
+        }
+        public static void SearchIngreds()
+        {
+            Console.Clear();
+            Console.Write("Ingredients you looking for(must be connected with comma ','):");
+            string SearchI = Console.ReadLine();
+            string[] arrayIngreds = SearchI.Split(new char[] {','});//cheese.10kg,lettuce.5kg,oil.10L,minced garlic.6L,french baguette.5kg,cloves.2kg
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string recipename = line.Split('@')[0];
+                string category = line.Split('@')[1].Split('$')[0];
+                string description = line.Split('@')[1].Split('$')[1].Split('#')[1];
+                string ingreds = line.Split('@')[1].Split('$')[1].Split('#')[0];
+                string[] ingredarray = ingreds.Split(new char[] { ',' });
+                string[] Name = new string[ingredarray.Length];
+                string[] Quantity = new string[ingredarray.Length];
+                for (int j = 0; j < ingredarray.Length; j++)
+                {
+                    Name[j] = ingredarray[j].Split('.')[0];
+                    Quantity[j] = ingredarray[j].Split('.')[1];
+                }
+                for (int k = 0; k < Name.Length; k++)
+                {
+                    for (int x = 0; x < arrayIngreds.Length; x++)
+                    {
+                        if (arrayIngreds[x]==Name[k])
+                        {
+                            Console.WriteLine($"{recipename}");
+                        }
+                    }
+                }
+            }
+        }
+        public static void menu()
         {
             int ch;
             Console.WriteLine("\t   ============");
@@ -34,13 +125,14 @@ namespace The_CookBook
             Console.WriteLine("\t   ============");
             Console.WriteLine("---------------Menu---------------");
             Console.WriteLine("(1) Add new recipe");
-            Console.WriteLine("(2) Show All the recipe");
-            Console.WriteLine("(3) Search and edit the recipe");
-            Console.WriteLine("(4) Exit from the program");
+            Console.WriteLine("(2) Show All the recipes");
+            Console.WriteLine("(3) Search Recipe");
+            Console.WriteLine("(4) Search Category");
+            Console.WriteLine("(5) Search Ingredients");
+            Console.WriteLine("(6) Exit!");
             Console.WriteLine("----------------------------------");
-            Console.WriteLine(" enter your choice:");
+            Console.Write(" enter your choice: ");
             ch=int.Parse(Console.ReadLine());
-            
             switch(ch)
             {
                 case 1:
@@ -49,12 +141,22 @@ namespace The_CookBook
                     break;
                 case 2:
                     Console.WriteLine("ok2");
+                    AllRecipes();
                     break;
                 case 3:
                     Console.WriteLine("ok3");
+                    SearchRecipe();
                     break;
                 case 4:
-                    Console.WriteLine("exist");
+                    Console.WriteLine("ok4");
+                    SearchCategory();
+                    break;
+                case 5:
+                    Console.WriteLine("ok5");
+                    SearchIngreds();
+                    break;
+                case 6:
+                    Console.WriteLine("Exit!");
                     break;
                 default: 
                     Console.WriteLine("something wrong!");
@@ -63,34 +165,7 @@ namespace The_CookBook
         }
         static void Main(string[] args)
         {
-            // Read a text file line by line.
-            
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\ZAKARIA\source\repos\The CookBook\recipes.txt");
-
-            // Display the file contents by using a foreach loop.
-            System.Console.WriteLine("Contents of recipes.txt = ");
-            foreach (string line in lines)
-            {
-                // Use a tab to indent each line of the file.
-                Console.WriteLine("\t" + line);
-            }
-            Console.ReadLine();
-
-            //string textFile = "recipes.txt";
-            /* string[] lines = File.ReadAllLines(textFile);
-             if (File.Exists(textFile))
-             {
-                 // Read a text file line by line.  
-                 lines = File.ReadAllLines(textFile);
-                 foreach (string line in lines)
-                     Console.WriteLine(line);
-             }
-             else
-             {
-                 Console.Write("walo makhedamch hadchi");
-             }
-             //menu();
-            */
+            menu();
         }
     }
 }
